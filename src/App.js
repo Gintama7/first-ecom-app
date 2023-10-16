c1a5af7f28e2f68ae5bf43b804137139b9c77d4e
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-10-16 11:33:33 +0530

    completed app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
+import ProductForm from './components/ProductForm';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [formData,setFormData] = useState('');
+  const [electronicsInfo,setElectronicsInfo] = useState([]);
+const [foodInfo,setFoodInfo] = useState([]);
+const [skinInfo,setSkinInfo] = useState([]);
+
+
+
+
+  const formHandler=(item)=>{
+   
+    setFormData(item); 
+    
+      if(item.category === 'electronics')
+      {
+         setElectronicsInfo((prev)=> [item,...prev]);
+        
+      }
+      else if(item.category === 'food')
+      {
+        setFoodInfo((prev)=>[item,...prev]);
+      }
+      else if(item.category === 'skincare')
+      {
+        setSkinInfo((prev)=>[item,...prev]);
+      }
+      
+      
+  }
+  
+
+  const deleteHandler=(e,key)=>{
+    const mainParent = e.target.parentElement.parentElement;
+    mainParent.removeChild(mainParent.lastElementChild);
+    localStorage.removeItem(key);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <ProductForm formHandler={formHandler}/>
+      <h1>Products</h1>
+      <ul id="table1List"><h2>Electronic Items</h2> 
+  {electronicsInfo.map((item)=>(
+    <li key={item.id}>{item.product} - {item.category} - {item.price} <button onClick={(e)=>deleteHandler(e,item.id)}>Delete</button></li>
+  ))}
+   </ul>
+   <ul id="table2List"><h2>Food Items</h2> 
+  {foodInfo.map((item)=>(
+    <li key={item.id}>{item.product} - {item.category} - {item.price} <button onClick={(e)=>deleteHandler(e,item.id)}>Delete</button></li>
+  ))}
+   </ul>
+   <ul id="table3List"><h2>Skincare Items</h2> 
+  {skinInfo.map((item)=>(
+    <li key={item.id}>{item.product} - {item.category} - {item.price} <button onClick={(e)=>deleteHandler(e,item.id)}>Delete</button></li>
+  ))}
+   </ul>
     </div>
   );
 }
---
+++ src/components/ProductForm.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+
+const ProductForm = (props) => {
+    const [idInput,setIdInput] = useState('');
+    const [priceInput,setPriceInput] = useState('');
+    const [productInput,setProductInput] = useState('');
+    const [categoryOption,setCategoryOption] = useState('');
+
+const idChangeHandler=(e)=>{
+    setIdInput(e.target.value);
+}
+const priceChangeHandler=(e)=>{
+    setPriceInput(e.target.value);
+}
+
+const dishChangeHandler=(e)=>{
+    setProductInput(e.target.value);
+}
+
+const categoryChangeHandler=(e)=>{
+    setCategoryOption(e.target.value);
+}
+
+    const submitHandler =(e)=>{
+        e.preventDefault();
+       const obj = {id:+idInput,price:+priceInput,product:productInput,category:categoryOption};
+       let obj1= JSON.stringify(obj);
+       localStorage.setItem(idInput,obj1);
+       props.formHandler(obj);
+       setIdInput('');
+       setProductInput('');
+       setPriceInput('');
+       setCategoryOption("choose an option");
+    }
+
+  return (
+    <form onSubmit={submitHandler}>
+        <label htmlFor="productId">Product ID</label>
+        <input type="text" name="productId" id="" onChange={idChangeHandler} value={idInput}/>
+        <label htmlFor="price">Selling Price</label>
+        <input type="number" name='price'onChange={priceChangeHandler} value={priceInput} />
+        <label htmlFor="product">Product Name</label>
+        <input type="text" name="product" id="" onChange={dishChangeHandler} value={productInput}/>
+        <label htmlFor="categoryId">Choose a Category</label>
+        <select name="categoryId" id="" onChange={categoryChangeHandler} value={categoryOption}>
+            <option value="choose an option">Categories</option>
+            <option value="electronics">Electronic Items</option>
+            <option value="food">Food Items</option>
+            <option value="skincare">Skincare Items</option>
+        </select>
+        <button type='submit'>Add Product</button>
+    </form>
+  )
+}
+
+export default ProductForm
